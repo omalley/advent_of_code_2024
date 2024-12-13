37795d16d7035b6dc21a887a4a9a6236a764c8e0
Owen O'Malley <omalley@apache.org>
2024-12-13 11:55:36 -0800

    move to array2d for day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,4 @@
+use array2d::Array2D;
 use itertools::Itertools;
 use smallvec::SmallVec;
 
@@ -70,17 +71,17 @@ impl Guard {
 
 #[derive(Clone,Debug)]
 pub struct Grid {
-  floor: Vec<Vec<Floor>>,
+  floor: Array2D<Floor>,
   guard: Guard,
   bounds: Coordinate,
 }
 
 impl Grid {
 
-  fn find_guard(floor: &[Vec<Floor>]) -> Option<Guard> {
-    floor.iter().enumerate()
+  fn find_guard(floor: &Array2D<Floor>) -> Option<Guard> {
+    floor.rows_iter().enumerate()
         .find_map(|(y, line)|
-            line.iter().enumerate()
+            line.enumerate()
                 .find_map(|(x, flr)| match flr {
                   Floor::Guard(facing) =>
                     Some(Guard{position: Coordinate{x: x as i32, y: y as i32},
@@ -89,23 +90,19 @@ impl Grid {
                 }))
   }
 
-  fn get(&self, position: &Coordinate) -> Option<Floor> {
-    if (0..self.bounds.x).contains(&position.x) &&
-        (0..self.bounds.y).contains(&position.y) {
-      Some(self.floor[position.y as usize][position.x as usize])
-    } else {
-      None
-    }
+  fn get(&self, position: &Coordinate) -> Option<&Floor> {
+    self.floor.get(position.y as usize, position.x as usize)
   }
 
-  fn get_mut(&mut self, position: &Coordinate) -> &mut Floor {
-    &mut self.floor[position.y as usize][position.x as usize]
+  fn get_mut(&mut self, position: &Coordinate) -> Option<&mut Floor> {
+    self.floor.get_mut(position.y as usize, position.x as usize)
   }
 
   pub fn from_string(input: &str) -> Result<Grid, String> {
-    let floor: Vec<Vec<Floor>> = input.lines()
+    let array: Vec<Vec<Floor>> = input.lines()
         .map(|line| line.chars().map(Floor::from_char).try_collect()).try_collect()?;
-    let bounds = Coordinate { x: floor[0].len() as i32, y: floor.len() as i32};
+    let floor: Array2D<Floor> = Array2D::from_rows(&array).map_err(|e| format!("{e}"))?;
+    let bounds = Coordinate { x: floor.column_len() as i32, y: floor.row_len() as i32};
     let guard = Self::find_guard(&floor).ok_or("No guard found")?;
     Ok(Grid { floor, guard, bounds })
   }
@@ -121,20 +118,21 @@ struct SquareState {
 }
 
 struct WalkState {
-  state: Vec<Vec<SquareState>>,
+  state: Array2D<SquareState>,
   current: Guard,
   square_count: usize,
 }
 
 impl WalkState {
 
+  #[inline]
   fn get_mut(&mut self, position: &Coordinate) -> &mut SquareState {
-    &mut self.state[position.y as usize][position.x as usize]
+    self.state.get_mut(position.y as usize,position.x as usize).unwrap()
   }
 
   fn from_grid(grid: &Grid) -> Self {
-    let state = vec![vec![SquareState::default(); grid.bounds.x as usize];
-                    grid.bounds.y as usize];
+    let state = Array2D::filled_with(SquareState::default(),
+                                     grid.bounds.y as usize, grid.bounds.x as usize);
     let current = grid.guard.clone();
     WalkState{state, current, square_count: 1}
   }
@@ -148,7 +146,7 @@ impl WalkState {
           self.current.turn_right();
         } else {
           // If we haven't been to this square, bump up the count.
-          if self.get_mut(&forward_coordinate).stack.is_empty() && forward_floor == Floor::Empty {
+          if self.get_mut(&forward_coordinate).stack.is_empty() && *forward_floor == Floor::Empty {
             self.square_count += 1;
           }
           let old = self.current.clone();
@@ -177,10 +175,10 @@ impl WalkState {
   fn place_block(&mut self, grid: &mut Grid, place: &Coordinate) -> bool {
     // It should be empty or the guard, but we can't place it on the guard.
     // If this is not the first time to this place, we can't test it now.
-    if grid.get(place) != Some(Floor::Empty) || !self.get_mut(place).stack.is_empty() {
+    if *grid.get(place).unwrap_or(&Floor::Full) != Floor::Empty || !self.get_mut(place).stack.is_empty() {
       return false
     }
-    *grid.get_mut(place) = Floor::Full;
+    *(grid.get_mut(place).unwrap()) = Floor::Full;
     let orig_guard = self.current.clone();
     self.get_mut(&orig_guard.position).stack.push(orig_guard.clone());
     let is_loop = self.walk_is_loop(grid);
@@ -190,7 +188,7 @@ impl WalkState {
         panic!("Crap");
       }
     }
-    *grid.get_mut(place) = Floor::Empty;
+    *(grid.get_mut(place).unwrap()) = Floor::Empty;
     is_loop
   }
 }
